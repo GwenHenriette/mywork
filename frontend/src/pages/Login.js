d3b118c77eb0d6b8591ee9ad4dd10bef912493d9
Gwendolyne Henriette <gwendolynehenriette@Gwendolynes-MacBook-Pro.local>
2024-02-28 05:31:28 -0400

    I removed an import statement in Login.js file

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -15,7 +15,6 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './Login.css';
 import Config from "../config/config";
-import apiurl from '../middleware/apiurl';
 
 const theme = createTheme({
   palette: {
