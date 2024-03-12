62f45214270f5bd8aabeadba455ead7633a1a7d6
Gwendolyne Henriette <gwendolynehenriette@tf694.wpa.dal.ca>
2024-03-12 14:23:37 -0300

    The change i made to the config.js file was not committed, so i am committing my config.js file changes once more

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,25 +1,24 @@
 const getAPI_URL = () => {
   if (process.env.NODE_ENV.toLowerCase() === 'development') {
-    if (process.env.REACT_APP_DEV_API_URL === null) {
-      console.log('\nNO `REACT_APP_DEV_API_URL` DEFINED\n')
-      return null
+    if (typeof process.env.REACT_APP_DEV_API_URL === 'undefined') {
+      console.log('\nNO `REACT_APP_DEV_API_URL` DEFINED\n');
+      return null;
     }
-    return process.env.REACT_APP_DEV_API_URL
-  }
-  else if (process.env.NODE_ENV.toLowerCase() === 'production') {
-    if (process.env.REACT_APP_PROD_API_URL === null) {
-      console.log('\nNO `REACT_APP_PROD_API_URL` DEFINED\n')
-      return null
+    return process.env.REACT_APP_DEV_API_URL;
+  } else if (process.env.NODE_ENV.toLowerCase() === 'production') {
+    if (typeof process.env.REACT_APP_PROD_API_URL === 'undefined') {
+      console.log('\nNO `REACT_APP_PROD_API_URL` DEFINED\n');
+      return null;
     }
-    return process.env.REACT_APP_PROD_API_URL
+    return process.env.REACT_APP_PROD_API_URL;
   }
 
-  console.log('\nNO NODE_ENV DEFINED\n')
-  return null
-}
+  console.log('\nNO NODE_ENV DEFINED\n');
+  return null;
+};
 
 const config = {
-  API_URL: getAPI_URL()
-}
+  API_URL: getAPI_URL(),
+};
 
-export default config
+export default config;
